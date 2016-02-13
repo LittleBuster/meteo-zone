38d592b4b0b504387a147252a91e06804cd6b1d1
LittleBuster <DenisovS21@gmail.com>
2016-02-13 06:08:46 +0600

    add license

--- pi-client/lcd.h
+++ pi-client/lcd.h
@@ -1,3 +1,14 @@
+/* Meteo-zone server
+ *
+ * Copyright (C) 2016 Sergey Denisov.
+ * Written by Sergey Denisov aka LittleBuster (DenisovS21@gmail.com)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public Licence 3
+ * as published by the Free Software Foundation; either version 3
+ * of the Licence, or (at your option) any later version.
+ */
+
 #ifndef __LCD_H__
 #define __LCD_H__
 
